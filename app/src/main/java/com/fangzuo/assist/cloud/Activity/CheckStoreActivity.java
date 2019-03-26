562622b636808644db54fd2075ef38c8ae5929aa
lins <753392431@qq.com>
2019-03-26 17:08:24 +0800

    下推界面修改，程序启动优化
    下推显示未验数量，出库单添加时请求基本数量和库存数量并再查看显示并汇总；程序启动后，回到桌面再次点击图标进入时，回到刚退出界面

---
+++ app/src/main/java/com/fangzuo/assist/cloud/Activity/CheckStoreActivity.java
@@ -0,0 +1,13 @@
+package com.fangzuo.assist.cloud.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckStoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_store);
+    }
+}
